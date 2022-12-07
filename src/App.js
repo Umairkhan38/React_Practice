6260734d8340d9bf41078e416f7f09bf14813eab
Umair Khan <uk125896@gmail.com>
2022-12-07 21:53:14 +0530

    commiting all

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Home from './home';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>This is Home Page Functional Component</h1>
+        <Home />
+      </header>
+    </div>
+  );
+}
+
+export default App;
