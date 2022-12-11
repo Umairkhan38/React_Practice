700a7570f9bd1420722e7da6480d30576a30bfad
Umair Khan <uk125896@gmail.com>
2022-12-11 20:24:09 +0530

    Commiting all

--- src/App.js
+++ src/App.js
@@ -1,13 +1,37 @@
-import Home from './home';
-import './App.css';
+import Properties from "./Components/Properties.js";
+import "./App.css";
+import { Product } from "./Components/Product";
+import StateHandle from "./Components/StateHandle.js";
+import List from "./Components/List.js";
+import Form from "./Components/Form.js";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./Components/Navbar.js";
+import CompA from "./Components/LifeCycle/CompA.js";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>This is Home Page Functional Component</h1>
-        <Home />
-      </header>
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Navbar />} />
+          <Route path="/lifeCycle" element={<CompA />} />
+
+          <Route
+            path="/Product"
+            element={
+              <Product
+                pdname="Webcam"
+                desc="This is a Web camera"
+                price="1250"
+              />
+            }
+          />
+
+          <Route path="/List" element={<List />} />
+          <Route path="/Form" element={<Form />} />
+          <Route path="/StateHandle" element={<StateHandle />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
